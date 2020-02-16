8f623356ab8ee393efa980d2d81afd8fd0e27557
H4LL <adam.hall@napier.ac.uk>
2020-02-16 00:30:25 +0000

    update to experiment script

--- experiment/run_experiment.sh
+++ experiment/run_experiment.sh
@@ -2,6 +2,7 @@
 
 #Build node and gateway images
 cd ~/PyGrid
+mkdir experiment/results
 conda activate pysyft
 sed -i -e 's,git+git://github.com/OpenMined/PySyft@master,git+git://github.com/OpenMined/PySyft@79d1507cd90435a0203b07fc670699507fade7c5,g' ./app/websocket/requirements.txt
 sudo docker build -t openmined/grid-node ./app/websocket/
@@ -14,16 +15,23 @@ sudo docker-compose up &
 SUBNET=$!
 
 #DISTRIBUTE DATA
-while true; do sudo docker stats --no-stream | ts '[%H:%M:%S]' | tee --append connect_stats.txt; sleep 0.5; done &
+while true; do sudo docker stats --no-stream | ts '[%H:%M:%S]' | tee --append experiment/results/connect_stats.txt; sleep 0.5; done &
 STATS=$!
-python python/connect_nodes.py > connect_nodes_output.txt
+sudo tcpdump -i lo -vv > experiment/results/connect.pcap &
+PCAP=$!
+python experiment/python/connect_nodes.py | tee experiment/results/connect_nodes_output.txt
 sudo kill -9 "$STATS"
+sudo kill -9 "$PCAP"
+
 
 #TRAIN ON DATA
-while true; do sudo docker stats --no-stream | ts '[%H:%M:%S]' | tee --append train_stats.txt; sleep 0.5; done &
+while true; do sudo docker stats --no-stream | ts '[%H:%M:%S]' | tee --append experiment/results/train_stats.txt; sleep 0.5; done &
 STATS=$!
-python python/learn_from_grid.py > learn_from_grid_output.txt
+sudo tcpdump -i lo -vv > experiment/results/train.pcap &
+PCAP=$!
+python experiment/python/learn_from_grid.py | tee experiment/results/learn_from_grid_output.txt
 sudo kill -9 "$STATS"
+sudo kill -9 "$PCAP"
 
 #TEAR DOWN SUBNET
 sudo kill -9 "$SUBNET"
